d137d649c32f5b3e14be950dbe01b41f46ee0fa2
Sarabjeet Singh <iamsarabjeetsingh3@gmail.com>
2025-06-08 21:32:21 +0530

    Updated Backend

--- backend/server.js
+++ backend/server.js
@@ -3,29 +3,41 @@ import cors from "cors";
 import "dotenv/config";
 import connectDB from "./config/mongodb.js";
 import connectCloudinary from "./config/cloudinary.js";
+
+// Routers
 import userRouter from "./routes/userRoute.js";
 import productRouter from "./routes/productRoute.js";
 import cartRouter from "./routes/cartRoute.js";
 import orderRouter from "./routes/orderRoute.js";
+import modifyUiRouter from "./routes/modifyUiRoute.js";
+import settingsRouter from "./routes/settingsRoutes.js";
+import footerRouter from "./routes/footerRoute.js";
+import categoryRouter from "./routes/categoryRoute.js";
 
-//App Config
+// App Config
 const app = express();
 const port = process.env.PORT || 4000;
 connectDB();
 connectCloudinary();
 
-//Middlewares
+// Middlewares
 app.use(express.json());
 app.use(cors());
 
-//Api endpoint
+// API Endpoints
 app.use("/api/user", userRouter);
 app.use("/api/product", productRouter);
 app.use("/api/cart", cartRouter);
 app.use("/api/order", orderRouter);
+app.use("/api/modify", modifyUiRouter);
+app.use("/api/settings", settingsRouter);
+app.use("/api/footer", footerRouter);
+app.use("/api/category", categoryRouter);
 
+// Default route
 app.get("/", (req, res) => {
   res.send("API Working");
 });
 
+// Start server
 app.listen(port, () => console.log("Server started on PORT : " + port));
